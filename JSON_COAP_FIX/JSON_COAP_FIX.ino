fc6add18911b86824a0fa751d35d66850b0153f3
gufir <gunawan.firdaus99@ymail.com>
2025-02-07 21:42:55 +0700

    refactor(coap&mqtt): add rtos for mqtt and coap

--- JSON_COAP_FIX/JSON_COAP_FIX.ino
+++ JSON_COAP_FIX/JSON_COAP_FIX.ino
@@ -1,64 +1,65 @@
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <coap-simple.h>
 #include <WiFiUdp.h>
 #include <DHT.h>
 #include <Adafruit_Sensor.h>
 #include <ArduinoJson.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
 #define DHTPIN 2
 #define DHTTYPE DHT22
 
-// WiFi SSID and Pass
 const char* ssid = "ASDE";
 const char* password = "1234566678";
 
-//callback
-void callback_response(CoapPacket &packet, IPAddress ip, int port);
-
 WiFiUDP udp;
 Coap coap(udp);
-
 DHT dht(DHTPIN, DHTTYPE);
 
-void WiFiSetup(){
-  // Connect to WiFi network
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
+void WiFiSetup(void *parameter) {
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+        Serial.print(".");
+    }
+    Serial.println("\nWiFi connected");
+    vTaskDelete(NULL);
+}
+
+void CoAPTask(void *parameter) {
+    while (1) {
+        float temperature = dht.readTemperature();
+        float humidity = dht.readHumidity();
+
+        DynamicJsonDocument doc(1024);
+        doc["Sensor"] = "DHT22";
+        doc["Protocol"] = "CoAP";
+        doc["Humidity"] = humidity;
+        doc["Temperature"] = temperature;
+
+        String jsonString;
+        serializeJson(doc, jsonString);
+        Serial.println(jsonString);
+
+        coap.post(IPAddress(192, 168, 43, 192), 5683, "SensorData", jsonString.c_str());
+        
+        vTaskDelay(300000 / portTICK_PERIOD_MS); // Delay 5 menit
+    }
 }
 
 void setup() {
-  Serial.begin(115200);
-  dht.begin();
-  WiFiSetup();
-  coap.start();
+    Serial.begin(115200);
+    dht.begin();
+    coap.start();
+    
+    xTaskCreate(WiFiSetup, "WiFiSetup", 4096, NULL, 1, NULL);
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+    
+    xTaskCreate(CoAPTask, "CoAPTask", 4096, NULL, 1, NULL);
 }
 
 void loop() {
-
-  int interval = 300000;
-  const long currentmillis = millis();
-
-  if(currentmillis<=interval){
-    delay(2000);
-    float temperature = dht.readTemperature();
-    float humidity = dht.readHumidity();
-
-    //JSON Object
-    DynamicJsonDocument doc(1024);
-    doc["Sensor"] = "DHT22";
-    doc["Protocol"] = "CoAP";
-    doc["Humidity"] = humidity;
-    doc["Temperature"] = temperature;
- 
-    String jsonString;
-    serializeJson(doc, jsonString);
-    Serial.println(jsonString);
-
-    coap.post(IPAddress(192,168,43,192),5683,"SensorData",jsonString.c_str());
-  }
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
 }
--- JSON_MQTT_FIX/JSON_MQTT_FIX.ino
+++ JSON_MQTT_FIX/JSON_MQTT_FIX.ino
@@ -1,96 +1,84 @@
-
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 #include <DHT.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
-// PIN DHT22
 #define DHTPIN 2          // Pin D4 ESP
 #define DHTTYPE DHT22      // DHT22 Sensor
 
-// MQTT Broker and User 
 const char* mqtt_server = "192.168.43.192";
 const char* mqtt_username = "pi";
 const char* mqtt_password = "raspberry";
 const char* mqtt_topic = "sensorDHT22";
 
-// Koneksi WiFi 
 const char* ssid = "ASDE";
 const char* password = "1234566678";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-
 DHT dht(DHTPIN, DHTTYPE);
 
-
-void WiFiSetup(){
-  // Connect to WiFi network
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
-}
-
-void BrokerConnect(){
-
-  // Connect to MQTT broker
-  client.setServer(mqtt_server,1883);
-  
-  while (!client.connected()) {
-    Serial.println("Connecting to MQTT broker...");
-    if (client.connect("ESP8266Client", mqtt_username, mqtt_password)) {
-      Serial.println("Connected to MQTT broker");
-    } else {
-      Serial.print("Failed with state ");
-      Serial.print(client.state());
-      delay(2000);
+void WiFiSetup(void *parameter) {
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+        Serial.print(".");
     }
-  }
-  
+    Serial.println("\nWiFi connected");
+    vTaskDelete(NULL);
 }
 
+void BrokerConnect(void *parameter) {
+    client.setServer(mqtt_server, 1883);
+    while (!client.connected()) {
+        Serial.println("Connecting to MQTT broker...");
+        if (client.connect("ESP8266Client", mqtt_username, mqtt_password)) {
+            Serial.println("Connected to MQTT broker");
+        } else {
+            Serial.print("Failed with state ");
+            Serial.println(client.state());
+            vTaskDelay(2000 / portTICK_PERIOD_MS);
+        }
+    }
+    vTaskDelete(NULL);
+}
+
+void SensorTask(void *parameter) {
+    while (1) {
+        float temperature = dht.readTemperature();
+        float humidity = dht.readHumidity();
+
+        DynamicJsonDocument doc(1024);
+        doc["Sensor Node"] = "Sensor Node 1";
+        doc["Protocol"] = "MQTT";
+        doc["Temperature"] = temperature;
+        doc["Humidity"] = humidity;
+        
+        String jsonString;
+        serializeJson(doc, jsonString);
+        Serial.println(jsonString);
+        client.publish(mqtt_topic, jsonString.c_str());
+        
+        vTaskDelay(300000 / portTICK_PERIOD_MS); // Delay 5 menit
+    }
+}
 
 void setup() {
-  Serial.begin(115200);
-  dht.begin();
-  WiFiSetup();
-  BrokerConnect();
-
+    Serial.begin(115200);
+    dht.begin();
+    
+    xTaskCreate(WiFiSetup, "WiFiSetup", 4096, NULL, 1, NULL);
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+    
+    xTaskCreate(BrokerConnect, "BrokerConnect", 4096, NULL, 1, NULL);
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+    
+    xTaskCreate(SensorTask, "SensorTask", 4096, NULL, 1, NULL);
 }
 
 void loop() {
-  int interval = 300000;
-  const long currentmillis = millis();
-
-  if(currentmillis<=interval){
-
-    // Read Data Sensor DHT22
-    delay(2000); //Sensor Response Need Delay 2 s
-    float temperature = dht.readTemperature();
-    float humidity = dht.readHumidity();
-
-    // Convert to JSON
-    DynamicJsonDocument doc(1024);
-    doc["Sensor Node"] = "Sensor Node 1";
-    doc["Protocol"] = "MQTT";
-    doc["Temperature"] = temperature;
-    doc["Humidity"] = humidity;
-    String jsonString;
-    serializeJson(doc, jsonString);
-    Serial.println(jsonString);
-
-    //Publish to MQTT
-    client.publish(mqtt_topic,jsonString.c_str());
-  }
-  else{
-    // Disconnecting from Broker
-    client.disconnect();
-    Serial.println("Client Disconnected");
-    
-  }
-
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
 }
